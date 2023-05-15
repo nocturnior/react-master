81b9e6fa5d2217a5eb03c9661cf5b2e31f94d198
nocturnior <ssuperr_@naver.com>
2023-05-16 01:49:39 +0900

    :construction: [RRD] React-Router-Dom : error 컨트롤

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Home from './screens/Home';
 import About from './screens/About';
 import Root from './Root';
+import NotFound from './screens/NotFound';
+import ErrComponent from './components/ErrorComponent';
 
 // array 형식으로
 const router = createBrowserRouter([
@@ -14,12 +16,14 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />,
+        errorElement: <ErrComponent />,
       },
       {
         path: 'about',
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
