f6e293d6d1e7bdcc5d88edbb3adb79ba67a817a8
nocturnior <ssuperr_@naver.com>
2023-05-17 02:05:46 +0900

    :construction: [Coin] : 데이터 타입가져와서 적용하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -7,15 +7,88 @@ interface RouteState {
     name: string;
   };
 }
+interface ITag {
+  coin_counter: number;
+  ico_counter: number;
+  id: string;
+  name: string;
+}
+
+// alt + shift + i : 우측으로 포커싱
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  // tags: ITag[];
+  // team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
 
 export default function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const { state } = useLocation() as RouteState;
-  console.log(state.name);
 
-  // const location = useLocation();
-  // const coinName = location.state as RouteState;
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, [coinId]); // coinId가 변한다면 그떄 실행
 
   return (
     <Container>
@@ -23,7 +96,7 @@ export default function Coin() {
         {/* state가 존재하면 name을 가져오고 아니면 로딩중~ 👉 코인으로 바로 들어가면 안보여짐 */}
         <Title>{state?.name || '로딩중❗'}</Title>
       </Header>
-      {loading ? <Loader>로딩중❗</Loader> : null}
+      {loading ? <Loader>로딩중❗</Loader> : <span>priceInfo?.quotes.USD</span>}
     </Container>
   );
 }
