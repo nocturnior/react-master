15b2c1e71a0a0640eb8e0cc69984e34f84f948c4
nocturnior <ssuperr_@naver.com>
2023-05-16 14:21:41 +0900

    :bulb: [RRD] + comment : useOutletContext 추가 설명

--- src/screens/users/Followers.tsx
+++ src/screens/users/Followers.tsx
@@ -9,3 +9,6 @@ export default function Followers() {
 
   return <h1>여긴~ {nameOfMyUser}의 followers</h1>;
 }
+
+// 종종 상위 경로는 하위 경로와 state 또는 기타 값을 공유합니다.
+// 원하는 경우 context provider를 만들 수 있지만 Outlet에 기본 제공되는 context를 사용할 수도 있습니다.
