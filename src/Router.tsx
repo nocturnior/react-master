a3895e9f279c55b5d47f0f919d8b2cd2b016366b
nocturnior <ssuperr_@naver.com>
2023-05-16 14:11:14 +0900

    :construction: [RRD] React-Router-Dom : Outlet 사용해보기
    '/'를 쓰면 절대경로가 된다 → '/'를 빼고 쓰면 현재 있는 URL다음에 경로를 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Root from './Root';
 import NotFound from './screens/NotFound';
 import ErrComponent from './components/ErrorComponent';
 import User from './screens/users/User';
+import Followers from './screens/users/Followers';
 
 // array 형식으로
 const router = createBrowserRouter([
@@ -26,8 +27,13 @@ const router = createBrowserRouter([
       {
         path: 'users/:userId',
         element: <User />,
+        children: [
+          {
+            path: 'followers',
+            element: <Followers />,
+          },
+        ],
       },
-      
     ],
     errorElement: <NotFound />,
   },
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,13 +1,21 @@
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import { users } from '../../db';
 
 export default function User() {
   //  <Route path >와 일치하는 현재 URL에서 동적 매개변수의 key/value 쌍 객체를 반환합니다. 하위 경로는 상위 경로에서 모든 매개변수를 상속합니다.
   const { userId } = useParams();
+  console.log('🚀 ⁝ User ⁝ userId:', userId);
 
   return (
-    <h1>
-      유저의 아이디는 {userId} 이고, 이름은 {users[Number(userId) - 1].name} 입니다 ㅋ
-    </h1>
+    <>
+      <h1>
+        유저의 아이디는 {userId} 이고, 이름은 {users[Number(userId) - 1].name} 입니다 ㅋ
+      </h1>
+
+      <hr />
+      {/* '/'를 쓰면 절대경로가 된다 → '/'를 빼고 쓰면 현재 있는 URL다음에 경로를 추가 */}
+      <Link to='followers'>팔로워</Link>
+      <Outlet />
+    </>
   );
 }
