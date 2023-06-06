5a60520f8d733706f56ec258918c64c192a21bfc
nocturnior <ssuperr_@naver.com>
2023-06-06 18:54:50 +0900

    :construction: [RHF] 정규표현식을 이용해서 validation Check + 에러핸들링(메세지)

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -33,32 +33,83 @@ export default function TodoList() {
   );
 } */
 
+type IFormData = {
+  errors: {
+    email: {
+      message: string;
+    };
+  };
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  password: string;
+  passwordConfirm: string;
+};
+
 export default function TodoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: '@naver.com',
+    },
+  });
 
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors);
 
   return (
     <>
       <form onSubmit={handleSubmit(onValid)} style={{ display: 'flex', flexDirection: 'column' }}>
-        <input {...register('email', { required: true })} placeholder='이메일' />
-        <input {...register('firstName', { required: true })} placeholder='성' />
-        <input {...register('lastName', { required: true })} placeholder='이름' />
-        <input {...register('userName', { required: true, minLength: 3 })} placeholder='닉네임' />
+        <input
+          {...register('email', {
+            required: '이메일이 필요합니다',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: '네이버 형식 이메일만 사용할 수 있습니다.',
+            },
+          })}
+          placeholder='이메일'
+        />
+        <span style={{ fontSize: '12px' }}>{errors?.email?.message}</span>
+
+        <input {...register('firstName', { required: '쓰슈' })} placeholder='성' />
+        <span style={{ fontSize: '12px' }}>{errors?.firstName?.message}</span>
+
+        <input {...register('lastName', { required: '쓰쇼' })} placeholder='이름' />
+        <span style={{ fontSize: '12px' }}>{errors?.lastName?.message}</span>
+
+        <input
+          {...register('userName', {
+            required: '쓰쇼',
+            minLength: { value: 3, message: '닉넴이 넘 짧아용 ㅋ' },
+          })}
+          placeholder='닉네임'
+        />
+        <span style={{ fontSize: '12px' }}>{errors?.userName?.message}</span>
+
         <input
           {...register('password', {
-            required: 'password is required',
+            required: '비번을 필수로 입력',
             minLength: { value: 5, message: '비밀번호가넘짧다' },
           })}
           placeholder='비밀번호'
         />
+        <span style={{ fontSize: '12px' }}>{errors?.password?.message}</span>
+
         <input
-          {...register('passwordConfirm', { required: true, minLength: 5 })}
+          {...register('passwordConfirm', {
+            required: '비번을 필수로 입력',
+            minLength: { value: 5, message: '비밀번호가 다르다' },
+          })}
           placeholder='비밀번호 확인'
         />
+        <span style={{ fontSize: '12px' }}>{errors?.passwordConfirm?.message}</span>
         <button>추가</button>
       </form>
     </>
