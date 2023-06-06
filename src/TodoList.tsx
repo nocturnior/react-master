f7ef514391f6019c701df1317d710af164248ae9
nocturnior <ssuperr_@naver.com>
2023-06-06 16:37:52 +0900

    :sparkles: [RHF] React-Hook-Form 사용하기 이전 코드
    각 form마다 코드를 적어줘야합니다! 귀찮오...

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 
 export default function TodoList() {
   const [todo, setTodo] = useState('');
+  const [todoErr, setTodoErr] = useState('');
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = e;
+    setTodoErr('');
     setTodo(value);
   };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(todo);
+    if (todo.length < 10) {
+      return setTodoErr('할일이 넘 짧닼ㅋㅋ');
+    }
+    console.log('등록!');
   };
 
   return (
@@ -20,6 +25,7 @@ export default function TodoList() {
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={todo} type='text' placeholder='머할건지' />
         <button>추가</button>
+        {todoErr !== '' ? todoErr : null}
       </form>
     </>
   );
